690ee210018f637c65d56d4bea19eccf27e8850c
julian zuleta gutierrez <julian65zuleta@hotmail.com>
2025-09-13 16:11:24 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,4 +1,17 @@
 import requests
 from bs4 import BeautifulSoup
+
 urljob = "https://co.computrabajo.com/trabajos-de-python-en-cali"
 urlresponse = requests.get(urljob)
+soup = BeautifulSoup(urlresponse.text, "html.parser")
+job_list = soup.find_all("article", class_="box_offer")
+jobscraped_data = []
+for jobarticle in job_list:
+    jobfinderarticle = jobarticle.find("h2")
+    if jobfinderarticle:
+        link_tag = jobfinderarticle.find("a")
+        company_title = jobarticle.find("p")
+        company_title_text = company_title.text.strip()
+        if link_tag:
+            job_title = link_tag.text.strip()
+            job_link = link_tag["href"]
