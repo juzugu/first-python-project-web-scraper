b6631901ebcfba1b1aed322b2b18d698af384a83
julian zuleta gutierrez <julian65zuleta@hotmail.com>
2025-09-14 12:16:11 -0500

    Update scraper.py

--- scraper.py
+++ scraper.py
@@ -1,6 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 import sqlite3
+con = sqlite3.connect("jobsscrapingjuzugu.db")
+cur = con.cursor()
+cur.execute(""" CREATE TABLE IF NOT EXISTS jobs_computrabajo (
+    title TEXT,
+    company TEXT,
+    link TEXT UNIQUE
+)""")
+
 urljob = "https://co.computrabajo.com/trabajos-de-python-en-cali"
 urlresponse = requests.get(urljob)
 soup = BeautifulSoup(urlresponse.text, "html.parser")
@@ -18,4 +26,6 @@ for jobarticle in job_list:
             job_title = link_tag.text.strip()
             job_link = link_tag["href"]
             job_data = {"title" :job_title , "company":company_title_justtext , "link": job_link}
-            jobscraped_data.append(job_data)
+"INSERT OR IGNORE INTO jobs_computrabajo (title, company, link) VALUES (?, ?, ?)"
+(job_data["title"], job_data["company"], job_data["link"])
+cur.execute()
